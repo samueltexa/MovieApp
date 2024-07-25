b88f7f455ee0ce7c794a535151d4eaff46044429
samueltexa <woolardsamuels@gmail.com>
2024-07-25 11:40:30 +0300

    add movies screen

---
+++ src/screens/MoviesScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MoviesScreen = () => {
+  return (
+    <View>
+      <Text>MoviesScreen</Text>
+    </View>
+  )
+}
+
+export default MoviesScreen
+
+const styles = StyleSheet.create({})
