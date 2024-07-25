65fcb45a46cfe66d674370ba8d4cf88eaa2a6a44
samueltexa <woolardsamuels@gmail.com>
2024-07-25 11:40:02 +0300

    add colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,3 +3,5 @@ export const PRIMARY_COLOR = '#FFFFFF'; // white
 export const SECONDARY_COLOR = '#000000'; //black
 
 export const TEXT_SECONDARY_COLOR = '#009245' // green
+
+export const ERROR_COLOR = '#da2e20' // red
