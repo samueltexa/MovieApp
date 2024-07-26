156e7228786549e65596e3c9d117e17269ab54a9
samueltexa <woolardsamuels@gmail.com>
2024-07-26 14:07:16 +0300

    add movies screen

--- src/screens/MoviesScreen.tsx
+++ src/screens/MoviesScreen.tsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
+import Reusable_TextInput from '../components/ui_components/Reusable_TextInput'
 
-const MoviesScreen = () => {
+const MoviesScreen = ({navigation}:{navigation:any}) => {
+
+  React.useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerRight:()=>(
+        <Reusable_TextInput custom_styles={{margin:10, padding:5}} placeholdeText='Search here'/>
+      )
+    });
+
+  },[navigation]);
+
   return (
     <View>
-      <Text>MoviesScreen</Text>
     </View>
   )
 }
