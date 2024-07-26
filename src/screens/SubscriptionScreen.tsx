c4855907344d71d17f73a290eff206ea6b8cd73c
samueltexa <woolardsamuels@gmail.com>
2024-07-26 14:07:56 +0300

    add subscription screen

---
+++ src/screens/SubscriptionScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SubscriptionScreen = () => {
+  return (
+    <View>
+      <Text>SubscriptionScreen</Text>
+    </View>
+  )
+}
+
+export default SubscriptionScreen
+
+const styles = StyleSheet.create({})
